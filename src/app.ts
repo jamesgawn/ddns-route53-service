53ae3e603e443c97e2d64a5f750dd1319ce61314
jamesgawn <james@gawn.uk>
2020-04-25 09:35:07 +0100

    Added simple logging.

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import Logger from 'bunyan';
 import Koa from 'koa';
 import KoaJson from 'koa-json';
 import KoaRouter from 'koa-router';
+import KoaBodyParser from 'koa-bodyparser';
 import { AppUtils } from "./utils/apputils";
 
 const port = AppUtils.normalisePort(3000);
@@ -19,7 +20,12 @@ router.get('/', async (ctx, next) => {
 
 const koa = new Koa();
 koa.use(KoaJson());
+koa.use(KoaBodyParser());
 koa.use(router.routes()).use(router.allowedMethods());
+koa.use( (ctx, next) => {
+    logger.info({msg: 'Got a request from %s for %s', ip: ctx.request.ip, path: ctx.path});
+    return next();
+});
 
 koa.listen(port, () => {
     logger.info(`server listening to http://localhost:${port}`);
