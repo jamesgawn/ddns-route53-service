957a2ad613a3932f313b538e0d7a0fdd91981c08
jamesgawn <james@gawn.uk>
2020-04-25 09:19:00 +0100

    Added option to change listening port via environment variable

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ import Logger from 'bunyan';
 import Koa from 'koa';
 import KoaJson from 'koa-json';
 import KoaRouter from 'koa-router';
+import { AppUtils } from "./utils/apputils";
+
+const port = AppUtils.normalisePort(3000);
 
 const logger = Logger.createLogger({
     name: "ddns-route53-service"
@@ -18,6 +21,6 @@ const koa = new Koa();
 koa.use(KoaJson());
 koa.use(router.routes()).use(router.allowedMethods());
 
-koa.listen(3000, () => {
-    logger.info('Server listening to http://localhost:3000');
+koa.listen(port, () => {
+    logger.info(`server listening to http://localhost:${port}`);
 });
