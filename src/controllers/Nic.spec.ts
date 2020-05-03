93b367806f08639e57c8c74d723a3a065bf46a26
jamesgawn <james@gawn.uk>
2020-05-03 21:55:29 +0100

    Restructure and add new nic/update endpoint

---
+++ src/controllers/Nic.spec.ts
@@ -0,0 +1,77 @@
+import { Nic } from "./Nic";
+import Logger from "bunyan";
+import {NextFunction, Request, Response} from "express";
+
+describe("Nic", () => {
+    let nic: Nic;
+    let testUser: string;
+    let testPass: string;
+    let logger: Logger;
+
+    beforeAll(() => {
+        testUser = "testUser";
+        testPass = "testPass";
+        logger = Logger.createLogger({
+            name: "test-logger"
+        });
+    });
+
+    beforeEach(() => {
+        process.env.SERVICE_USER = testUser;
+        process.env.SERVICE_PASS = testPass;
+    });
+
+    describe('constructor',  () => {
+        it('should successfully create if user and pass env vars are available', () => {
+            nic = new Nic(logger);
+            expect(nic).toBeInstanceOf(Nic);
+            expect(nic).not.toBeNull();
+        });
+        it('should throw an error if user env var is not available', () => {
+            delete process.env.SERVICE_USER;
+            try {
+                nic = new Nic(logger);
+            } catch (err) {
+                expect(err.message).toBe("No service credentials specified in environment variables.");
+            }
+        });
+        it('should throw an error if pass env var is not available', () => {
+            delete process.env.SERVICE_PASS;
+            try {
+                nic = new Nic(logger);
+            } catch (err) {
+                expect(err.message).toBe("No service credentials specified in environment variables.");
+            }
+        });
+    });
+
+    describe('update', () => {
+        let req: Request;
+        let res: Response;
+        let next: NextFunction;
+
+        const mockResStatus = jest.fn();
+        const mockNext = jest.fn();
+        beforeEach(() => {
+            nic = new Nic(logger);
+            req = {
+                headers: {
+                    authorisation: undefined
+                }
+            } as unknown as Request;
+            res = {
+                status: mockResStatus
+            } as unknown as Response;
+            next = jest.fn();
+        });
+        it('should update IP with valid credentials', () => {
+            req.headers.authorization = "something";
+            nic.update(req, res, mockNext);
+        });
+        it('should not update IP with missing authorisation header', () => {
+            nic.update(req, res, mockNext);
+            expect(mockResStatus).toBeCalledWith(401);
+            expect(mockNext).toBeCalledWith(new Error('Update failed due to missing authorisation header.'));
+        });
+    });
+});
--- src/utils/EnvVarUtil.ts
+++ src/utils/EnvVarUtil.ts
@@ -1,42 +1,28 @@
 import Logger from 'bunyan';
 
 export class EnvVarUtil {
-    cache: Map<string,string>;
     log: Logger;
 
     constructor (log: Logger) {
         this.log = log.child({ module: 'config-helper' });
-        this.cache = new Map<string, string>();
     }
 
-    get (name: string, errWhenMissing?: boolean) {
-        if (this.cache.has(name)) {
-            this.log.info('Retrieved ' + name + ' from Cache');
-            return this.cache.get(name);
+    get(name: string) {
+        const value = process.env[name];
+        if (value === undefined || value === "") {
+            const err = new Error('Failed to retrieve ' + name + ' from ENV Vars');
+            this.log.error({
+                err
+            }, 'Failed to retrieve ' + name + ' from ENV Vars');
+            return null;
         } else {
-            const value = process.env[name];
-            if (value === undefined || value === "") {
-                if (errWhenMissing || errWhenMissing === undefined) {
-                    const err = new Error('Failed to retrieve ' + name + ' from ENV Vars');
-                    this.log.error({
-                        err
-                    }, 'Failed to retrieve ' + name + ' from ENV Vars');
-                    throw err;
-                } else {
-                    this.log.info('Retrieved ' + name + ' from ENV Vars, but it was unavailable');
-                    return null;
-                }
-            } else {
-                this.log.info('Retrieved ' + name + ' from ENV Vars');
-                this.cache.set(name, value);
-                this.log.info('Saved ' + name + ' in Cache');
-                return value;
-            }
+            this.log.info('Retrieved ' + name + ' from ENV Vars');
+            return value;
         }
     }
 
     getWithDefault(name: string, defaultValue: string) {
-        const value = this.get(name, false);
+        const value = this.get(name);
         if (value === null) {
             this.log.info("Providing default value for " + name);
             return defaultValue;
